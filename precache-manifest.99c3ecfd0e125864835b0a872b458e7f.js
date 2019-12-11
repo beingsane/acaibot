40d4e6d0f5ef2982e1e44f36ab8c542a6f2e5d4c
root <you@example.com>
2019-12-11 11:34:16 -0300

    Updates

---
+++ precache-manifest.99c3ecfd0e125864835b0a872b458e7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7549d164d41489fb2ad017a32a98ad91",
+    "url": "/acaibot/index.html"
+  },
+  {
+    "revision": "3df4c0973608412890f2",
+    "url": "/acaibot/static/css/2.667739f9.chunk.css"
+  },
+  {
+    "revision": "4cc5e343920c0c2751de",
+    "url": "/acaibot/static/css/main.12eb45f2.chunk.css"
+  },
+  {
+    "revision": "3df4c0973608412890f2",
+    "url": "/acaibot/static/js/2.b31a95f5.chunk.js"
+  },
+  {
+    "revision": "cee38df0383683d48604da9772fd0c35",
+    "url": "/acaibot/static/js/2.b31a95f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cc5e343920c0c2751de",
+    "url": "/acaibot/static/js/main.ce219b52.chunk.js"
+  },
+  {
+    "revision": "5b222e9feaa2299eb5e2",
+    "url": "/acaibot/static/js/runtime-main.9965b67f.js"
+  },
+  {
+    "revision": "8512ab3f7b0ef891479ed2b4cfb36c71",
+    "url": "/acaibot/static/media/acai-background.8512ab3f.jpg"
+  }
+]);
